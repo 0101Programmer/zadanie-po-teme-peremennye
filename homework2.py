f6f824d836cf4b48b995e02acca0a08ab1160bfb
0101Programmer <baranov-v-k@yandex.ru>
2024-07-21 14:42:42 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,34 @@
+# first_name = 'выполненных ДЗ:'
+# dz_count = 12
+# second_name = 'затраченные часы:'
+# hr_count = 1.5
+# third_name = 'Python'
+# fourth_name = 'время на 1 задание:'
+# time_count = hr_count / dz_count
+# print('Курс:', third_name + ',', first_name, dz_count, ',', second_name, hr_count, ',', fourth_name, time_count, 'часа.')
+
+
+# a = 'выполненных ДЗ:'
+# b = 12
+# c = 'затраченные часы:'
+# d = 1.5
+# f = 'Python'
+# i = 'время на 1 задание:'
+# g = d / b
+# print('Курс:', f + ',', a, b, ',', c, d, ',', i, g, 'часа.')
+
+first_name = 'выполненных ДЗ:'
+a = first_name
+dz_count = 12
+b = dz_count
+second_name = 'затраченные часы:'
+c = second_name
+hr_count = 1.5
+d = hr_count
+third_name = 'Python'
+f = third_name
+fourth_name = 'время на 1 задание:'
+i = fourth_name
+time_count = hr_count / dz_count
+g = time_count
+print('Курс:', f + ',', a, b, ',', c, d, ',', i, g, 'часа.')
